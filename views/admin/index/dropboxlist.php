bc79ba7e501067bd7d4493314c15e581b4b847d8
Douglas Knox <dknox@wustl.edu>
2013-03-27 13:07:36 -0500

    added direct links to Dropbox files; hover preview for image files

--- views/admin/index/dropboxlist.php
+++ views/admin/index/dropboxlist.php
@@ -1,9 +1,16 @@
 <?php
     $filePath = PLUGIN_DIR . DIRECTORY_SEPARATOR . 'Dropbox' . DIRECTORY_SEPARATOR . 'files';
     $fileNames = dropbox_dir_list($filePath);
+
+    function isImage($file)
+    {
+        if(@getimagesize($file) == false)
+            return false;
+        else
+            return true;
+    }
     
 ?>
-
 
 <?php if (!$fileNames): ?>
     <p>No files have been uploaded to the dropbox.</p>
@@ -59,14 +66,30 @@
 
             jQuery('.dropbox-js').show();
             jQuery('#dropbox-show-all').hide();
+            
+            jQuery(".filename").hover(function(){
+                    jQuery('#imgprev').empty();
+                    jQuery('#imgprev').append('<img src="' + jQuery(this).attr('href')+ '"/>');
+                    jQuery('#imgprev img').animate({'opacity':0.7},400);
+                },
+                function(){
+                    jQuery('#imgprev img').animate({'opacity': 0.1},300,
+                        function(){ jQuery(this).remove();}
+                    );
+                    
+            });
         });
     </script>
-    
+    <style>
+        #imgprev {max-width: 400px; margin-left: 300px; top: 100px; position: fixed;}
+        #imgprev img {max-height: 700px; max-width: 400px; opacity: 0.1; border: 3px solid #888; border-radius: 5px;}
+    </style>
     <p class="dropbox-js" style="display:none; vertical-align:baseline; margin-bottom:0">
         Filter files by name:
         <input id="dropbox-file-filter" name="dropbox-file-filter" class="textinput" style="font-size:1em">
         <a href="#" id="dropbox-show-all" style="vertical-align:baseline">Show All</a>
     </p>
+    <div id="imgprev"></div>
     <table>
         <colgroup>
             <col style="width: 2em">
@@ -79,7 +102,14 @@
         </thead>
         <tbody id="dropbox-file-checkboxes">
         <?php foreach ($fileNames as $fileName): ?>
-            <tr><td><input type="checkbox" name="dropbox-files[]" value="<?php echo html_escape($fileName); ?>"/></td><td><?php echo html_escape($fileName); ?></td></tr>
+            <tr><td><input type="checkbox" name="dropbox-files[]" value="<?php echo html_escape($fileName); ?>"/></td><td>
+                
+                <a 
+                <?php if (isImage($filePath ."/". $fileName)) { echo 'class="filename"';} ?> 
+                
+                href="<?php echo WEB_PLUGIN . "/Dropbox/files/" . html_escape($fileName); ?>"><?php echo html_escape($fileName); ?></a>
+                
+                </td></tr>
         <?php endforeach; ?>
         </tbody>
     </table>
